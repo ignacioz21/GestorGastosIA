d63d48e72f58f38626f9db24c8eef03a320eee0f
ignacioz21 <168688549+ignacioz21@users.noreply.github.com>
2025-06-11 14:15:49 -0600

    Se implemento la primera version de DB

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from src.routes.main import bp as main_bp
+from src.database import init_db
 
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = 'dev'
+
+    init_db()
     app.register_blueprint(main_bp)
     
     return app
---
+++ src/database/__init__.py
@@ -0,0 +1,41 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='gestorIA',
+            user = 'root',
+            password = '123456789'
+        )
+        return connection
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None
+    
+
+def init_db():
+    connection = get_db_connection()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS expenses (
+                                    ID INT AUTO_INCREMENT PRIMARY KEY,
+                                    NAME VARCHAR(100) NOT NULL,
+                                    AMOUNT DECIMAL NOT NULL,
+                                    DATE DATE NOT NULL,
+                                    CATEGORY VARCHAR(50),
+                                    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                )
+                            """)
+            connection.commit()
+            print("Database initialized successfully.")
+        except Error as e:
+            print(f"Error while initializing the database: {e}")
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed.")
